7143be46f17b3ef46948cb701ab9fd0844ab92bd
palys <tomek201992@gmail.com>
2015-05-06 21:38:57 +0200

    Add create database script

---
+++ siusMedicines/database/createDatabase.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE medicinesManager
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Polish_Poland.1250'
+       LC_CTYPE = 'Polish_Poland.1250'
+       CONNECTION LIMIT = -1;
