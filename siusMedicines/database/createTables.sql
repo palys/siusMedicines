3196dc5ab3e64b2784ada56cbaa3b6f51e2e29ab
palys <tomek201992@gmail.com>
2015-05-19 12:49:41 +0200

    Database schema updated

--- siusMedicines/database/createTables.sql
+++ siusMedicines/database/createTables.sql
@@ -22,3 +22,61 @@ WITH (
 );
 ALTER TABLE users
   OWNER TO postgres;
+  
+CREATE TABLE patients
+(
+  id SERIAL,
+  name character varying(45) NOT NULL,
+  surname character varying(45) NOT NULL,
+  username character varying(45) NOT NULL references users (username),
+  CONSTRAINT patients_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE patients
+  OWNER TO postgres;
+  
+CREATE TABLE doctors
+(
+  id SERIAL,
+  name character varying(45) NOT NULL,
+  surname character varying(45) NOT NULL,
+  username character varying(45) NOT NULL references users (username),
+  CONSTRAINT doctors_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE doctors
+  OWNER TO postgres;
+  
+CREATE TABLE prescriptions
+(
+  id SERIAL,
+  patient_id integer references patients (id),
+  doctor_id integer references doctors (id),
+  medicine_name character varying(45) references medicines (name),
+  CONSTRAINT prescriptions_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE prescriptions
+  OWNER TO postgres;
+  
+CREATE TABLE portions
+(
+  id SERIAL,
+  unit character varying(45),
+  size real,
+  take_time date,
+  taken boolean,
+  prescription_id integer references prescriptions (id),
+  CONSTRAINT portions_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE portions
+  OWNER TO postgres;
